5c11a40d65bea035ab8be4fc3b13fbc2f1b04472
AdanNavarro <2121100566@soy.utj.edu.mx>
2022-11-28 03:58:30 -0600

    Update head.blade.php

--- resources/views/template/elements/head.blade.php
+++ resources/views/template/elements/head.blade.php
@@ -18,7 +18,11 @@
 
 <link rel="stylesheet" href=" {{ asset('assets/css/lightbox.css') }} ">
 
-
+<link rel="apple-touch-icon" sizes="180x180" href=" {{ asset('assets/images/apple-touch-icon.png') }} ">
+<link rel="icon" type="image/png" sizes="32x32" href=" {{ asset('assets/images/favicon-32x32.png') }} ">
+<link rel="icon" type="image/png" sizes="16x16" href=" {{ asset('assets/images/favicon-16x16.png') }} ">
+<link rel="manifest" href=" {{ asset('assets/images/site.webmanifest') }} ">
+<link rel="mask-icon" href=" {{ asset('assets/images/safari-pinned-tab.svg') }} " color="#5bbad5">
 
 <style>
     .form-control:focus,
