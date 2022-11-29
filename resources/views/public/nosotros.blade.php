a2074ffbfada717ec36fb891054a55f3ad52dd63
AdanNavarro <2121100566@soy.utj.edu.mx>
2022-11-28 20:39:53 -0600

    Update nosotros.blade.php

--- resources/views/public/nosotros.blade.php
+++ resources/views/public/nosotros.blade.php
@@ -10,8 +10,7 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="inner-content">
-                        <h2>About Our Company</h2>
-                        <span>Awesome, clean &amp; creative HTML5 Template</span>
+                        <h2>Sobre nuestro Boutique</h2>
                     </div>
                 </div>
             </div>
@@ -30,17 +29,12 @@
                 </div>
                 <div class="col-lg-6">
                     <div class="right-content">
-                        <h4>About Us &amp; Our Skills</h4>
-                        <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod kon tempor incididunt
-                            ut labore.</span>
+                        <h4>Sobre nosotros</h4>
+                        <span> Somos una tienda boutique que vende ropa a la moda, tanto para niños y niñas, mujeres y hombres, así como tambien vendemos accesorios para que siempre estes a la moda. </span>
                         <div class="quote">
                             <i class="fa fa-quote-left"></i>
-                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiuski smod kon tempor
-                                incididunt ut labore.</p>
+                            <p>Nuestro objetivo es que nuestros clientes siempre que entren salgan con un producto de nuestra tienda, y no por insistencia, si no por gusto de ellos al ver la ropa o accesorios que ofrecemos</p>
                         </div>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod kon tempor incididunt ut
-                            labore et dolore magna aliqua ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                            nisi ut aliquip.</p>
                         <ul>
                             <li><a href="#"><i class="fa fa-facebook"></i></a></li>
                             <li><a href="#"><i class="fa fa-twitter"></i></a></li>
@@ -60,8 +54,8 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-heading">
-                        <h2>Our Amazing Team</h2>
-                        <span>Details to details is what makes Hexashop different from the other themes.</span>
+                        <h2>Nuestro Grandioso Equipo</h2>
+                        <span>Encargados del boutique, productos, distribución y marketing</span>
                     </div>
                 </div>
                 <div class="col-lg-4">
@@ -80,8 +74,8 @@
                             <img src="assets/images/team-member-01.jpg">
                         </div>
                         <div class="down-content">
-                            <h4>Ragnar Lodbrok</h4>
-                            <span>Product Caretaker</span>
+                            <h4>Susana Torres</h4>
+                            <span>Marketing</span>
                         </div>
                     </div>
                 </div>
@@ -101,8 +95,8 @@
                             <img src="assets/images/team-member-02.jpg">
                         </div>
                         <div class="down-content">
-                            <h4>Ragnar Lodbrok</h4>
-                            <span>Product Caretaker</span>
+                            <h4>Ronaldo Ismael</h4>
+                            <span>Director general</span>
                         </div>
                     </div>
                 </div>
@@ -122,8 +116,8 @@
                             <img src="assets/images/team-member-03.jpg">
                         </div>
                         <div class="down-content">
-                            <h4>Ragnar Lodbrok</h4>
-                            <span>Product Caretaker</span>
+                            <h4>Juan Carlos Villareal</h4>
+                            <span>Encargado de productos</span>
                         </div>
                     </div>
                 </div>
@@ -138,32 +132,29 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-heading">
-                        <h2>Our Services</h2>
-                        <span>Details to details is what makes Hexashop different from the other themes.</span>
+                        <h2>Misión, Visión y Objetivos</h2>
+                        <span>Con esto compartimos nuestra visión a futuro para que te conozcas mas sobre nosotros y lo que hacemos</span>
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="service-item">
-                        <h4>Synther Vaporware</h4>
-                        <p>Lorem ipsum dolor sit amet, consecteturti adipiscing elit, sed do eiusmod temp incididunt ut
-                            labore, et dolore quis ipsum suspend.</p>
+                        <h4>Misón</h4>
+                        <p>Ofrecer a nuestros clientes productos de calidad, a precios cómodos que cumplan con sus necesidades y exigencias, abarcando sus gustos de acuerdo a su estilo de ver y vivir la vida.</p>
                         <img src="assets/images/service-01.jpg" alt="">
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="service-item">
-                        <h4>Locavore Squidward</h4>
-                        <p>Lorem ipsum dolor sit amet, consecteturti adipiscing elit, sed do eiusmod temp incididunt ut
-                            labore, et dolore quis ipsum suspend.</p>
+                        <h4>Visión</h4>
+                        <p>Ser una empresa  líder y reconocida en la venta de ropa Rony Boutique, lograr también extendernos y crear  nuestras cadenas de almacenes, proporcionando cada día más un servicio de excelencia a nuestros clientes y que al mismo tiempo nos permitan competir en el mercado nacional con los mejores precios del mercado</p>
                         <img src="assets/images/service-02.jpg" alt="">
                     </div>
                 </div>
                 <div class="col-lg-4">
                     <div class="service-item">
-                        <h4>Health Gothfam</h4>
-                        <p>Lorem ipsum dolor sit amet, consecteturti adipiscing elit, sed do eiusmod temp incididunt ut
-                            labore, et dolore quis ipsum suspend.</p>
-                        <img src="assets/images/service-03.jpg" alt="">
+                        <h4>Objetivo</h4>
+                        <p>Ofrecer ropa de mejor calidad satisfaga las  necesidades y gustos del mercado potencial, vender ropa a la moda que cumpla con los requerimientos de estilo en el segmento  del mercado.</p>
+                        <img src="assets/images/explore-image-01.jpg" alt="">
                     </div>
                 </div>
             </div>
@@ -171,61 +162,5 @@
     </section>
     <!-- ***** Services Area Ends ***** -->
 
-    <!-- ***** Subscribe Area Starts ***** -->
-    <div class="subscribe">
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-8">
-                    <div class="section-heading">
-                        <h2>By Subscribing To Our Newsletter You Can Get 30% Off</h2>
-                        <span>Details to details is what makes Hexashop different from the other themes.</span>
-                    </div>
-                    <form id="subscribe" action="" method="get">
-                        <div class="row">
-                            <div class="col-lg-5">
-                                <fieldset>
-                                    <input name="name" type="text" id="name" placeholder="Your Name"
-                                        required="">
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-5">
-                                <fieldset>
-                                    <input name="email" type="text" id="email" pattern="[^ @]*@[^ @]*"
-                                        placeholder="Your Email Address" required="">
-                                </fieldset>
-                            </div>
-                            <div class="col-lg-2">
-                                <fieldset>
-                                    <button type="submit" id="form-submit" class="main-dark-button"><i
-                                            class="fa fa-paper-plane"></i></button>
-                                </fieldset>
-                            </div>
-                        </div>
-                    </form>
-                </div>
-                <div class="col-lg-4">
-                    <div class="row">
-                        <div class="col-6">
-                            <ul>
-                                <li>Store Location:<br><span>Sunny Isles Beach, FL 33160, United States</span></li>
-                                <li>Phone:<br><span>010-020-0340</span></li>
-                                <li>Office Location:<br><span>North Miami Beach</span></li>
-                            </ul>
-                        </div>
-                        <div class="col-6">
-                            <ul>
-                                <li>Work Hours:<br><span>07:30 AM - 9:30 PM Daily</span></li>
-                                <li>Email:<br><span>info@company.com</span></li>
-                                <li>Social Media:<br><span><a href="#">Facebook</a>, <a
-                                            href="#">Instagram</a>, <a href="#">Behance</a>, <a
-                                            href="#">Linkedin</a></span></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- ***** Subscribe Area Ends ***** -->
 
 @endsection
